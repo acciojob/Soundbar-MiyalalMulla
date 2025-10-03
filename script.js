a28d13916ad589470010d55532d23c1e572fcbb2
MiyalalMulla <mullamiyalal@gmail.com>
2025-10-03 10:48:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+let sounds = ['applause', 'boo','gasp','tada','victory',"wrong","stop"];
+const buttons = document.getElementById("buttons");
+
+sounds.forEach((sound)=>{
+	const audio = document.createElement("audio");
+	audio.id=sound;
+	audio.preload = 'auto';
+	document.body.appendChild(audio);
+
+	//visible button
+	const btn = document.createElement('button');
+	btn.class = "btn";
+	btn.textContent = sound;
+
+	btn.addEventListener("click",()=>{
+		stopSounds();
+	    const el = document.getElementById(sound);
+	    el.currentTime = 0;
+	    const p = el.play();
+	});
+	buttons.appendChild(btn);
+});
+const stopBtn = document.createElement("button");
+stopBtn.className = 'stop';
+stopBtn.textContent = 'stop';
+stopBtn.addEventListener("click", stopSounds);
+buttons.appendChild("stopBtn");
+function stopSounds(){
+	sounds.forEach((sound)=>{
+		const el = document.getElementById(sound);
+		el.pause();
+		el.currentTime = 0;
+	});
+}
